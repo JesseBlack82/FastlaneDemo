8cb69f688527b5cf42c8ed8c62a1a400c049a8d7
Jesse Black <jesse.black@stablekernel.com>
2015-09-08 14:30:40 -0400

    Updated project to build from multiple schemes

--- FastlaneDemo/ViewController.swift
+++ FastlaneDemo/ViewController.swift
@@ -9,9 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var brand: String {
+        get {
+        var b = ""
+        
+        #if JB
+        b = "Jesse Black"
+        #endif
+        
+        #if JSB
+        b = "JSB"
+        #endif
+        
+        return b
+        }
+    }
+    
+    var backend: String {
+        get {
+            var b = ""
+            #if QA
+                b = "QA"
+            #endif
+            
+            #if PROD
+                b = "PROD"
+            #endif
+            
+            return b
+        }
+    }
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        helloLabel.text = "Hello \(backend) from \(brand)"
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
